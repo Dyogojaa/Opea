6ebbb882786be1eaa85bb475b542aa5f678dc0c2
Dyogo <dyogo@gmail.com>
2022-08-05 23:45:34 -0300

    Projeto Final Angular Finalização do projeto Single Page Application

--- src/app/opea/add-edit-opea/add-edit-opea.component.ts
+++ src/app/opea/add-edit-opea/add-edit-opea.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OpeaApiService } from 'src/app/opea-api.service';
 
 @Component({
   selector: 'app-add-edit-opea',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditOpeaComponent implements OnInit {
 
-  constructor() { }
+  clienteList$!:Observable<any[]>;
+  tipoEmpresaList$!:Observable<any[]>;
+
+  constructor(private service:OpeaApiService) { }
+
+  @Input() cliente:any;
+  id:number=0;
+  nomeEmpresa:string="";
+  tipoEmpresaID:number=0;
+
+  addCliente(){
+    var cliente = {
+      nomeEmpresa:this.nomeEmpresa,
+      tipoEmpresaID:this.tipoEmpresaID
+    }  
+    this.service.addCliente(cliente).subscribe(res =>{
+      var closeModalBtn = document.getElementById('add-edit-modal-close');
+      if(closeModalBtn)
+      {
+        closeModalBtn.click();
+      }
+
+      var showAddSuccess = document.getElementById("add-success-alert");
+      if (showAddSuccess){
+        showAddSuccess.style.display='block';      
+      }
+
+      setTimeout(function(){
+        if(showAddSuccess)
+        {
+          showAddSuccess.style.display='none';      
+        }
+      },4000);
+    })
+  }
+
+  updateCliente(){
+    var cliente = {
+      id: this.id,
+      nomeEmpresa:this.nomeEmpresa,
+      tipoEmpresaID:this.tipoEmpresaID
+    } 
+    var id:number = this.id;
+    this.service.updateCliente(id,cliente).subscribe(res =>{
+      var closeModalBtn = document.getElementById('update-edit-modal-close');
+      if(closeModalBtn)
+      {
+        closeModalBtn.click();
+      }
+
+      var showupdateSuccess = document.getElementById("update-success-alert");
+      if (showupdateSuccess){
+        showupdateSuccess.style.display='block';      
+      }
+
+      setTimeout(function(){
+        if(showupdateSuccess)
+        {
+          showupdateSuccess.style.display='none';      
+        }
+      },4000);
+    })    
+  }
 
   ngOnInit(): void {
+    this.id = this.cliente.id;
+    this.nomeEmpresa = this.cliente.nomeEmpresa;
+    this.tipoEmpresaID = this.cliente.tipoEmpresaID;
+    this.tipoEmpresaList$ = this.service.getTipoEmpresaList();
+    this.clienteList$ = this.service.getClienteList();
+
   }
 
 }
--- src/app/opea/show-opea/show-opea.component.ts
+++ src/app/opea/show-opea/show-opea.component.ts
@@ -11,6 +11,7 @@ export class ShowOpeaComponent implements OnInit {
 
   clienteList$!:Observable<any[]>;
   tipoEmpresaList$!:Observable<any[]>;
+  clienteTiposList:any=[];
   
   //Map para mostrar a associação entre tabelas
   clientesTipoMap:Map<number, string> = new Map();
@@ -20,6 +21,69 @@ export class ShowOpeaComponent implements OnInit {
   ngOnInit(): void {
     this.clienteList$ = this.service.getClienteList();
     this.tipoEmpresaList$ =  this.service.getTipoEmpresaList();
+    this.atualizarTiposEmpresa();
+
   }
 
+
+  //Geral
+  modalTitle:string = 'Nova Empresa';
+  activateAddEditOpeaComponet:boolean =false;
+  Opea:any;
+
+  modalAdd(){
+    this.Opea= {
+       id:0,
+       nomeEmpresa:null,
+       tipoEmpresaID:null 
+
+    }
+    this.modalTitle ="Nova Empresa";
+    this.activateAddEditOpeaComponet = true;        
+  }
+  
+  modalEdit(item:any){
+    this.Opea=item;
+    this.modalTitle ="Edição de Empresa";
+    this.activateAddEditOpeaComponet = true;        
+
+  }
+  
+  delete(item:any){
+      if(confirm(`você tem certeza que quer deletar o cliente ${item.id}?`))
+      {
+        this.service.deleteCliente(item.id).subscribe(res =>{
+          var closeModalBtn = document.getElementById('delete-edit-modal-close');
+          if(closeModalBtn)
+          {
+            closeModalBtn.click();
+          }
+    
+          var showdeleteSuccess = document.getElementById("delete-success-alert");
+          if (showdeleteSuccess){
+            showdeleteSuccess.style.display='block';      
+          }
+    
+          setTimeout(function(){
+            if(showdeleteSuccess)
+            {
+              showdeleteSuccess.style.display='none';      
+            }
+          },4000);
+        })            
+      }
+  }
+
+  modalClose(){
+    this.activateAddEditOpeaComponet = false;
+    this.clienteList$ = this.service.getClienteList();
+  }
+
+  atualizarTiposEmpresa(){
+    this.service.getTipoEmpresaList().subscribe(data => {
+      this.clienteTiposList = data;
+      for (let i = 0; i< data.length; i++){
+        this.clientesTipoMap.set(this.clienteTiposList[i].id, this.clienteTiposList[i].tipo)  
+      }    
+  })}
 }
